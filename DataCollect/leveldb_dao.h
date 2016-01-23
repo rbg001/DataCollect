72fe34d25a33d5a25e93c5ef8f4fc8aea9c4534b
robin_home <315789501@qq.com>
2016-01-23 23:27:35 +0800

    添加 LevelDBDao的头文件

---
+++ DataCollect/leveldb_dao.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#pragma execution_character_set("utf-8")
+// 本文件为utf-8 编码格式
+#ifndef LEVELDB_DAO_H
+#define LEVELDB_DAO_H
+class LevelDBDao
+{
+public:
+protected:
+private:
+};
+
+#endif
